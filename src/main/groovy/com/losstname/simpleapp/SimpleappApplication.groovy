af65b7bc6e9641a5ea7cf6ff9356458aaa4929aa
losstname <losstname@gmail.com>
2019-05-27 23:46:10 +0800

    Initial commit

---
+++ src/main/groovy/com/losstname/simpleapp/SimpleappApplication.groovy
@@ -0,0 +1,13 @@
+package com.losstname.simpleapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SimpleappApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SimpleappApplication, args)
+	}
+
+}
